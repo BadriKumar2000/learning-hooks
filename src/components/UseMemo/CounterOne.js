9baa099e09629baabf119b7fc815490b2bc73bd5
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-10-10 18:43:39 +0530

    we used useMemo to optimize the performance of button 2, using the useMemo we can cache the calcualteValue of function inside useMemo, the function only calls when the reactive variable of dependency array changes

--- src/components/UseMemo/CounterOne.js
+++ src/components/UseMemo/CounterOne.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 function CounterOne() {
   const [countOne, setCountOne] = useState(0);
@@ -9,18 +9,18 @@ function CounterOne() {
   const increaseTwo = () => {
     setCountTwo(countTwo + 1);
   };
-  const isEven = () => {
+  const isEven = useMemo(() => {
     let i = 0;
-    while (i < 200000000) {
+    while (i < 200000) {
       i++;
     }
     return countOne % 2 === 0;
-  };
+  }, [countOne]);
   return (
     <div>
       <div>
         <button onClick={increaseOne}>CountOne {countOne}</button>
-        <span>{isEven() ? "Even" : "Odd"}</span>
+        <span>{isEven ? "Even" : "Odd"}</span>
       </div>
       <div>
         <button onClick={increaseTwo}>CountTwo {countTwo}</button>

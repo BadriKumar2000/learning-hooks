38f55c1b8bfed5a8290496cfa68e83e9bdf9fa98
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-10-10 18:27:28 +0530

    created isEven function to display that countOne is even or odd

--- src/components/UseMemo/CounterOne.js
+++ src/components/UseMemo/CounterOne.js
@@ -9,10 +9,14 @@ function CounterOne() {
   const increaseTwo = () => {
     setCountTwo(countTwo + 1);
   };
+  const isEven = () => {
+    return countOne % 2 === 0;
+  };
   return (
     <div>
       <div>
         <button onClick={increaseOne}>CountOne {countOne}</button>
+        <span>{isEven() ? "Even" : "Odd"}</span>
       </div>
       <div>
         <button onClick={increaseTwo}>CountTwo {countTwo}</button>
